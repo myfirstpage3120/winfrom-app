d4239e558e8aeb2e07e1d4793293befaffaac089
Nihal Govardhan <68705084+myfirstpage3120@users.noreply.github.com>
2023-01-26 22:49:00 -0800

    Add files via upload

---
+++ NewWindowsFormsApp/NewWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace NewWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertWithSp = new System.Windows.Forms.Button();
+            this.btnDataWithSp = new System.Windows.Forms.Button();
+            this.txtEmpNo = new System.Windows.Forms.TextBox();
+            this.txtEmpName = new System.Windows.Forms.TextBox();
+            this.txtempSalary = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Moccasin;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.Color.Violet;
+            this.dataGridView1.Location = new System.Drawing.Point(200, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(473, 258);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Crimson;
+            this.label1.Location = new System.Drawing.Point(197, 291);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "EmployeeNo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(197, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "EmployeeName:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(197, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "EmployeeSalary:";
+            // 
+            // btnInsertWithSp
+            // 
+            this.btnInsertWithSp.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnInsertWithSp.Location = new System.Drawing.Point(445, 289);
+            this.btnInsertWithSp.Name = "btnInsertWithSp";
+            this.btnInsertWithSp.Size = new System.Drawing.Size(97, 37);
+            this.btnInsertWithSp.TabIndex = 4;
+            this.btnInsertWithSp.Text = "InsertWithSp";
+            this.btnInsertWithSp.UseVisualStyleBackColor = true;
+            this.btnInsertWithSp.Click += new System.EventHandler(this.btnInsertWithSp_Click);
+            // 
+            // btnDataWithSp
+            // 
+            this.btnDataWithSp.ForeColor = System.Drawing.Color.DarkOrange;
+            this.btnDataWithSp.Location = new System.Drawing.Point(578, 291);
+            this.btnDataWithSp.Name = "btnDataWithSp";
+            this.btnDataWithSp.Size = new System.Drawing.Size(95, 33);
+            this.btnDataWithSp.TabIndex = 5;
+            this.btnDataWithSp.Text = "DataWithSp";
+            this.btnDataWithSp.UseVisualStyleBackColor = true;
+            // 
+            // txtEmpNo
+            // 
+            this.txtEmpNo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtEmpNo.Location = new System.Drawing.Point(295, 287);
+            this.txtEmpNo.Name = "txtEmpNo";
+            this.txtEmpNo.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpNo.TabIndex = 6;
+            // 
+            // txtEmpName
+            // 
+            this.txtEmpName.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtEmpName.Location = new System.Drawing.Point(295, 327);
+            this.txtEmpName.Name = "txtEmpName";
+            this.txtEmpName.Size = new System.Drawing.Size(100, 20);
+            this.txtEmpName.TabIndex = 7;
+            this.txtEmpName.TextChanged += new System.EventHandler(this.txtEmpName_TextChanged);
+            // 
+            // txtempSalary
+            // 
+            this.txtempSalary.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.txtempSalary.Location = new System.Drawing.Point(295, 364);
+            this.txtempSalary.Name = "txtempSalary";
+            this.txtempSalary.Size = new System.Drawing.Size(100, 20);
+            this.txtempSalary.TabIndex = 8;
+            this.txtempSalary.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.ForeColor = System.Drawing.Color.LimeGreen;
+            this.btnUpdate.Location = new System.Drawing.Point(445, 349);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(97, 35);
+            this.btnUpdate.TabIndex = 9;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.ForeColor = System.Drawing.Color.Red;
+            this.btnDelete.Location = new System.Drawing.Point(578, 349);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(95, 35);
+            this.btnDelete.TabIndex = 10;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(851, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtempSalary);
+            this.Controls.Add(this.txtEmpName);
+            this.Controls.Add(this.txtEmpNo);
+            this.Controls.Add(this.btnDataWithSp);
+            this.Controls.Add(this.btnInsertWithSp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertWithSp;
+        private System.Windows.Forms.Button btnDataWithSp;
+        private System.Windows.Forms.TextBox txtEmpNo;
+        private System.Windows.Forms.TextBox txtEmpName;
+        private System.Windows.Forms.TextBox txtempSalary;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ NewWindowsFormsApp/NewWindowsFormsApp/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+        SqlDataReader reader = null;
+
+        public async void Show()
+        {
+            using (SqlCommand cmd = new SqlCommand("select * from tblEmployee", conn))
+            { //select the table from the database
+                conn.Open();   //we have opened the connection
+               reader = await cmd.ExecuteReaderAsync();
+                
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);                         //the data will get stored in this 
+                    dataGridView1.DataSource = dt;
+                    reader.Close();
+                Thread.Sleep(10000);
+                conn.Close();
+                
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)  // to display the data
+        {
+            conn = new SqlConnection("Data Source=POOLW42SLPC7838;Initial Catalog=\"Assignment 3\";Integrated Security=True");
+
+            Show();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private  void btnInsertWithSp_Click(object sender, EventArgs e)
+        {
+
+            conn = new SqlConnection("Data Source=POOLW42SLPC7838;Initial Catalog=\"Assignment 3\";Integrated Security=True");
+            using (cmd = new SqlCommand("sp_InsertEmployee", conn)) ; //we have to pass the stored  processor name
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                cmd.Parameters.Add("@empName", SqlDbType.VarChar, 20).Value = txtEmpName.Text;   //the txt value name should be same as text box value name
+                cmd.Parameters.Add("@empSalary", SqlDbType.Float).Value = txtempSalary.Text;
+                   
+
+                Task<int> x = cmd.ExecuteNonQueryAsync();
+                try
+                {
+                    cmd.ExecuteNonQueryAsync().Wait();
+                    
+                                       
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    MessageBox.Show("Data inserted successfully");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                //int x = cmd.ExecuteNonQuery();
+
+                //if (x > 0)
+                //{
+                //    MessageBox.Show("Row added succesfully");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Error");
+                //}
+                //conn.Close();
+                Show();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection("Data Source=POOLW42SLPC7838;Initial Catalog=\"Assignment 3\";Integrated Security=True");
+            using (cmd = new SqlCommand("sp_DeleteEmployee", conn)) ; //we have to pass the stored  processor name
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                cmd.Parameters.Add("@empNo", SqlDbType.Int).Value = Convert.ToInt32(txtEmpNo.Text);  //the value name should be same as text box value name
+                //cmd.Parameters.Add("@empSalary", SqlDbType.Float).Value = txtempSalary.Text;
+                //int x = cmd.ExecuteNonQuery();
+
+                Task<int> x = cmd.ExecuteNonQueryAsync();
+                try
+                {
+                    cmd.ExecuteNonQueryAsync().Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+
+                //if (x > 0)
+                //{
+                //    MessageBox.Show("Row deleted succesfully");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Error");
+                //}
+                //conn.Close();
+                Show();
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            conn = new SqlConnection("Data Source=POOLW42SLPC7838;Initial Catalog=\"Assignment 3\";Integrated Security=True");
+            using (cmd = new SqlCommand("sp_UpdateEmployee", conn)) ; //we have to pass the stored  processor name
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+                cmd.Parameters.Add("@empNo", SqlDbType.Int).Value = Convert.ToInt32(txtEmpNo.Text);  //the value name should be same as text box value name
+                cmd.Parameters.Add("@empSalary", SqlDbType.Float).Value = txtempSalary.Text;
+                cmd.Parameters.Add("@empName", SqlDbType.VarChar, 20).Value = txtEmpName.Text;
+                Task <int> x = cmd.ExecuteNonQueryAsync();
+
+
+
+                try
+                {
+                    cmd.ExecuteNonQueryAsync().Wait(500);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                finally
+                {
+                    conn.Close();
+                }
+
+                //if (x > 0)
+                //{
+                //    MessageBox.Show("Row Updated succesfully");
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Error");
+                //}
+
+                Show();
+            }
+        }
+
+        private void txtEmpName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
